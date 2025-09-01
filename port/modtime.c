31934e49ad647899f9e9a57224c8884918354c56
stnolting <stnolting@gmail.com>
2025-09-01 21:47:34 +0200

    [port] fix mp_time_localtime_get

--- port/modtime.c
+++ port/modtime.c
@@ -7,22 +7,21 @@
 #include <neorv32.h>
 
 /**********************************************************************//**
- * Return the localtime as an 8-tuple.
+ * Return the local time as an 8-tuple.
  **************************************************************************/
-mp_obj_t mp_time_localtime_get(void) {
+void mp_time_localtime_get(timeutils_struct_time_t *tm) {
+
   date_t date;
   neorv32_aux_unixtime2date(neorv32_clint_unixtime_get(), &date);
-  mp_obj_t tuple[8] = {
-    mp_obj_new_int(date.year),
-    mp_obj_new_int(date.month),
-    mp_obj_new_int(date.day),
-    mp_obj_new_int(date.hours),
-    mp_obj_new_int(date.minutes),
-    mp_obj_new_int(date.seconds),
-    mp_obj_new_int(date.weekday - 1),
-    mp_obj_new_int(timeutils_year_day(date.year, date.month, date.day)),
-  };
-  return mp_obj_new_tuple(8, tuple);
+
+  tm->tm_year = date.year;
+  tm->tm_mon  = date.month;
+  tm->tm_mday = date.day;
+  tm->tm_hour = date.hours;
+  tm->tm_min  = date.minutes;
+  tm->tm_sec  = date.seconds;
+  tm->tm_wday = date.weekday - 1;
+  tm->tm_yday = timeutils_year_day(date.year, date.month, date.day);
 }
 
 
@@ -30,7 +29,9 @@ mp_obj_t mp_time_localtime_get(void) {
  * Returns the number of seconds since Epoch.
  **************************************************************************/
 mp_obj_t mp_time_time_get(void) {
+
   date_t date;
   neorv32_aux_unixtime2date(neorv32_clint_unixtime_get(), &date);
+
   return mp_obj_new_int(timeutils_seconds_since_epoch(date.year, date.month, date.day, date.hours, date.minutes, date.seconds));
 }
